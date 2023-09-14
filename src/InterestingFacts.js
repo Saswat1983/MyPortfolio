8a1ea26df81096ae0b594f5f4dd6aaa1c26b07ae
saswat <saswat1983@gmail.com>
2023-09-14 17:27:55 -0400

    Minor updated

--- src/InterestingFacts.js
+++ src/InterestingFacts.js
@@ -12,8 +12,8 @@ export default function (imageProp) {
         <div class="col-sm-6 text-aligned-left">
             I am a very amicable person. I love to meet people, sometimes complete strangers, and get along quite well. <br/>
             I was born and brought up in India and recently moved to Canada.<br/>
-            I strongly believe that failure is a great teacher. The ability to stay optimistic and stare at the face of failure and learn for it, is a strong characteristic of an achiever. <br/>
-            Besides the fact that I still remember the periodic table, am a sucker for Botany and Zoology, I have never lost my interest in Programming. Some software problems have grown on me to the extent that I have even dreamt of them. And yes, I have found solutions in my sleep and implemented them after I wake up.<br/>
+            I strongly believe that failure is a great teacher. The ability to stay optimistic, stare at the face of failure and learn for it, is a strong characteristic of an achiever. <br/>
+            Besides the fact that I still remember the periodic table, am a sucker for Botany and Zoology, I have always been fascinated with Computer Programming. Some software problems have grown on me to the extent that I have even dreamt of them. And yes, I have found solutions in my sleep and implemented them after I wake up.<br/>
             And I almost forgot about it... I designed, developed in ReactJS and deployed this website on Azure in just 10 hours.<br/>
         </div>
 
